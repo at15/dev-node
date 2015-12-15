89519f84df639942a3fe83f1223a06852313b292
at15 <at15@outlook.com>
2015-12-15 17:28:10 +0800

    use environment for hadoop setup

--- script/hadoop/download.sh
+++ script/hadoop/download.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # config
-# TODO: use environment variable to override it
-HADOOP_DOWNLOAD=http://mirrors.cnnic.cn/apache/hadoop/common/stable/hadoop-2.7.1.tar.gz
+# use environment variable to override it http://stackoverflow.com/questions/307503/whats-the-best-way-to-check-that-environment-variables-are-set-in-unix-shellscr
+${HADOOP_DOWNLOAD:=http://mirrors.cnnic.cn/apache/hadoop/common/stable/hadoop-2.7.1.tar.gz}
+
+echo ${HADOOP_DOWNLOAD}
 
 # get the script path http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 pushd `dirname $0` > /dev/null
@@ -14,6 +16,7 @@ ORIGINAL_WD=${PWD}
 cd ${SCRIPTPATH}
 # switch to tarball directory
 cd ../../tarball
+
 # if we already have one, then we won't download it
 if [ -f hadoop-2.7.1.tar.gz ]; then
     echo "file already exist"
@@ -22,9 +25,5 @@ else
     wget ${HADOOP_DOWNLOAD}
 fi
 
-#echo ${SCRIPTPATH}
-#echo ${ORIGINAL_WD}
-#echo ${PWD}
-
+# go back to the old working directory
 cd ${ORIGINAL_WD}
-echo ${PWD}
